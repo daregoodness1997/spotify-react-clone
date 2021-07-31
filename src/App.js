2680992f415258abe2c29a1e599ad3ceda293b52
Dare Goodness <daregoodness@gmail.com>
2021-07-31 23:39:32 +0100

    spotify login flow

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Login from './components/Login';
+import { getTokenFromUrl } from './spotify';
 
 function App() {
+  const [token, setToken] = useState(null);
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = '';
+
+    const _token = hash.access_token;
+    if (_token) {
+      setToken(_token);
+    }
+    console.log('Token= ', token);
+  }, [token]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {/* Spotify Logo */}
+      {/* Login with spotify button0 */}
+      {token ? <h2>I'm Dare</h2> : <Login />}
     </div>
   );
 }
